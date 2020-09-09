dfa4ee28a19adec587f51a2c224449761a6a8dc2
nayokim <nayoung.kimpak@gmail.com>
2020-09-09 09:39:21 -0500

    content comp

--- src/index.js
+++ src/index.js
@@ -1,30 +1,43 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Header = (props) => {
+   return (
+     <h1>{props.name}</h1>
+   )
+}
+
+const Part = (props) => (
+  <p> {props.name} {props.exercises}</p>
+)
+
+const Content= (props) =>(
+  <Part name = {props.name} exercises = {props.exercises} />
+)
+
+
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = "State of a component"
-  const exercises3 = 14
+  // const part2 = 'Using props to pass data'
+  // const exercises2 = 7
+  // const part3 = "State of a component"
+  // const exercises3 = 14
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>
+      <Header name = {course}/>
+      <Content name = {part1} exercises = {exercises1}/>
+     
+    
+    
+
+
+      {/* <p>
         Number of Exercises {exercises1 + exercises2 + exercises3}
-      </p>
+      </p> */}
     </div>
   )
 }
